4b66fc82ea5f94587f1a2fbaed58b065eabdf5af
josueantuan <antuan_1996@hotmail.com>
2018-10-14 19:31:35 -0500

    first commit

---
+++ src/pages/mapa/mapa.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { GeolocalitationProvider } from "../../providers/geolocalitation/geolocalitation";
+import { MouseEvent } from '@agm/core';
+@IonicPage()
+@Component({
+  selector: 'page-mapa',
+  templateUrl: 'mapa.html',
+})
+export class MapaPage {
+  instruccion="";
+  lat:any;
+  lng:any;
+  latC:any;
+  lngC:any;
+  public origin: any;
+  public destination: any;
+  markers = [
+	  {
+		  latC:this.lat,
+		  lngC:this.lng,
+		  draggable: false
+	  }  
+  ]
+  constructor(public navCtrl: NavController, public navParams: NavParams, public geoloca:GeolocalitationProvider) {
+    this.instruccion = this.navParams.get('instruccion');
+    this.lat = this.geoloca.lat;
+    this.lng = this.geoloca.lng;
+  }
+  ngOnInit() {
+    console.log(this.instruccion);
+    this.lat = this.geoloca.lat;
+    this.lng = this.geoloca.lng;
+    this.getDirection();
+  }
+   
+  getDirection() {
+    this.origin = { lat: this.lat, lng: this.lng }
+    this.destination = { lat: -0.167765, lng: -78.489688 }
+    
+  }
+  mapClicked($event: MouseEvent) {
+    this.markers.push({
+      latC: $event.coords.lat,
+      lngC: $event.coords.lng,
+      draggable: false
+    });
+    this.latC= $event.coords.lat;
+    this.lngC= $event.coords.lng;
+    this.origin = { lat: this.lat, lng: this.lng }
+    this.destination = { lat: this.latC, lng: this.lngC }
+    console.log(this.latC,this.lngC);
+  }
+}
